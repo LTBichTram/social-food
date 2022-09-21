13eb365678d473084fd9057028b396050f481cff
Bich Tram <bichtramlt1911@gmail.com>
2022-09-21 23:06:29 +0700

    add .

---
+++ frontend/src/assets/images/index.js
@@ -0,0 +1,11 @@
+import login from "~/assets/images/login.png";
+import logoS from "~/assets/images/logoS.png";
+import form from "~/assets/images/form.png";
+
+const images = {
+  login: login,
+  logoS: logoS,
+  form: form,
+};
+
+export default images;
--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./Login.module.scss";
+import loginImg from "~/assets/images/login.png";
+import userImg from "~/assets/images/user.png";
+import lockImg from "~/assets/images/lock.png";
+import logoImg from "~/assets/images/logo.png";
+import images from "~/assets/images";
+
+const cx = classNames.bind(styles);
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className={cx("container")}>
+      <form action="" className={cx("card")}>
+        <img src={images.logoS} class={cx("logoS")} alt="" />
+        <h2>Đăng nhập</h2>
+        <div className={cx("input-group")}>
+          <input
+            type="text"
+            name="loginUser"
+            id="loginUser"
+            placeholder=" "
+            requiredb
+          />
+          <label for="loginUser">Email</label>
+        </div>
+        <div className={cx("input-group")}>
+          <input
+            type="password"
+            name="loginPassword"
+            id="loginPassword"
+            placeholder=" "
+            required
+          />
+          <label for="loginPassword">Mật khẩu</label>
+        </div>
+        <button type="submit" value="Login" className="btn">
+          Đăng nhập
+        </button>
+        <div className={cx("forgot-pw")}>Quên mật khẩu?</div>
+      </form>
+      <img src={images.login} alt="" />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
