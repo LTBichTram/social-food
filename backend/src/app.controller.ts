678923cbb5869d4aa1c079d219372bd45414ac8a
Ho Quang Linh <hoquanglinh2710@gmail.com>
2022-10-02 12:33:21 +0700

    update jwt

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Get } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Controller('api')
+export class AppController {
+  @Get()
+  sayHello(): string {
+    return 'Welocome to Api Food Social App';
+  }
+}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MessageModule } from './message/message.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+
 @Module({
-  imports: [AuthModule, UserModule, MessageModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    AuthModule,
+    UserModule,
+    MessageModule,
+  ],
+  controllers: [AppController],
 })
 export class AppModule {}
