a6fc246d1230b3f078ac874194164925c2177045
Ho Quang Linh <linh.hoquang@netpower.no>
2022-10-17 15:10:14 +0700

    update cloud services

--- backend/src/cloudinary/cloudinary.provider.ts
+++ backend/src/cloudinary/cloudinary.provider.ts
@@ -4,9 +4,9 @@ export const CloudinaryProvider = {
   provide: CLOUDINARY,
   useFactory: (): ConfigOptions => {
     return v2.config({
-      cloud_name: 'hoquanglinh',
-      api_key: '347722296972335',
-      api_secret: 'k51X9drlPdK3d3Puq2Vv4a_Vhik',
+      cloud_name: process.env.Cloud_Name,
+      api_key: process.env.Cloud_ApiKey,
+      api_secret: process.env.Cloud_ApiSecret,
     });
   },
 };
