e85cb82b22116cb9b9626fd15e5f9601b37c354d
Bich Tram <bichtramlt1911@gmail.com>
2022-10-23 16:38:05 +0700

    css sidebar

--- frontend/src/layouts/commons/Sidebar/Menu/MenuItem.js
+++ frontend/src/layouts/commons/Sidebar/Menu/MenuItem.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import classNames from "classnames/bind";
 
@@ -6,14 +5,13 @@ import styles from "./Menu.module.scss";
 
 const cx = classNames.bind(styles);
 
-const MenuItem = ({ to, title, icon, activeIcon, children }) => {
+const MenuItem = ({ to, title, icon, activeIcon }) => {
   return (
     <NavLink
       to={to}
       className={(nav) => cx("menu-item", { active: nav.isActive })}
     >
       <div className={cx("menu-wrapper")}>
-        {children}
         <span className={cx("icon")}>{icon}</span>
         <span className={cx("icon-active")}>{activeIcon}</span>
         <span className={cx("title")}>{title}</span>
