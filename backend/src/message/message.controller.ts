73c0634cbaab33d36bde03446d0dd6bb0d8f6da2
Bich Tram <bichtramlt1911@gmail.com>
2022-12-04 19:08:32 +0700

    revert

--- backend/src/message/message.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Post } from '@nestjs/common';
-
-@Controller('api/message')
-export class MessageController {
-  @Get()
-  sendMessage() {
-    return 'Send message';
-  }
-}
